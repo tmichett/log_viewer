84e5866b53d58f936a0bd7fd16c1931148a43b3f
Travis Michette <tmichett@redhat.com>
2025-05-09 19:20:45 -0400

    FIX: Testing of build process

--- rpmbuild/SOURCES/Build_App.sh
+++ rpmbuild/SOURCES/Build_App.sh
@@ -2,7 +2,10 @@
 ## Script to Build Executable Python App
 ## Author: tmichett@redhat.com
 
-pip3 install PyInstaller PyQt6 PyYAML
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv venv log_viewer
+source log_viewer/bin/activate
+uv pip install PyInstaller PyQt6 PyYA
 pyinstaller rpmbuild/SOURCES/log_viewer.spec
 
 cp rpmbuild/SOURCES/dist/log_viewer .
